5e5aec5290543b016b34fad62da99bf677e13ad4
MONIKA SINGH <92215187+Monika5S@users.noreply.github.com>
2023-04-12 16:43:00 +0530

    Designed and styled Navbar

---
+++ src/components/GlobalEvent.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function GlobalEvent() {
+  return (
+    <div className="">
+      <img src="" alt="event" />
+      <h2></h2>
+      <p></p>
+    </div>
+  );
+}
