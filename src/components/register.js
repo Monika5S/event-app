d429272700d3f928c833b3a81a98a9fc2a47c041
MONIKA SINGH <92215187+Monika5S@users.noreply.github.com>
2023-04-13 22:55:56 +0530

    working on register form

--- src/components/register.js
+++ src/components/register.js
@@ -1,14 +1,21 @@
 import React from "react";
 
 export default function Register() {
-  function Authorise(e) {
+  function handleSubmit(e) {
     e.preventDefault();
-    console.log("Authorised");
+    // const username = e.target.elements.username.value;
+    const password = e.target.elements.password.value;
+    const confirmPassword = e.target.elements.confirmPassword.value;
+    if (password != confirmPassword) {
+      alert("passwords do not match, please try again!");
+    } else {
+      alert("user registered successfully!");
+    }
   }
 
   return (
     <div>
-      <form className="mt-3" onSubmit={Authorise}>
+      <form className="mt-3" onSubmit={handleSubmit}>
         <div className="form-group mb-3">
           <input type="text" className="form-control" placeholder="username" />
         </div>
